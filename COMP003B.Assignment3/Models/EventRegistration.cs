edde7919e13ef25e12955d122154ddbcf0640745
romanvillagran <romanvillagran386@gmail.com>
2025-04-04 17:59:47 -0700

    added the required full name

--- COMP003B.Assignment3/Models/EventRegistration.cs
+++ COMP003B.Assignment3/Models/EventRegistration.cs
@@ -1,7 +1,14 @@
-﻿namespace COMP003B.Assignment3.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment3.Models
 {
     public class EventRegistration
     {
+        [Required(ErrorMessage = "Enter your name")]
+        [MinLength(3, ErrorMessage = "Name must contain 3 or more letters")]
+        public string FullName { get; set; }
+
 
     }
+
 }
